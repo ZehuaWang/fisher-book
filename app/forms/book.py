5e7557d1d10d35944751add966ce0128acfc4ae8
Zehua Wang <transforminottawa@gmail.com>
2019-10-06 16:02:52 -0400

    Fix the bug

--- app/forms/book.py
+++ app/forms/book.py
@@ -4,5 +4,5 @@ from wtforms.validators import Length, NumberRange
 
 class SearchForm(Form):
     q = StringField(validators=[Length(min=1, max=30)])
-    page = IntegerField(validators=[NumberRange(min=1, max=99)], deafault=1)
+    page = IntegerField(validators=[NumberRange(min=1, max=99)], default=1)
 
